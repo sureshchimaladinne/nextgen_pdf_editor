c5c1413947a2c3812ad205bbc173bc2298a82d34
sureshchimaladinne <sureshchimaladinne9063@gmail.com>
2025-11-30 07:47:03 +0530

    changed1

--- lib/controllers/drawing_controller.dart
+++ lib/controllers/drawing_controller.dart
@@ -27,10 +27,10 @@ class DrawingController extends ChangeNotifier {
   Color get getCurrentColor => _currentColor;
 
   /// Sets a new drawing color.
-  void setColor(Color color) {
-    _currentColor = color;
-    notifyListeners();
-  }
+  // void setColor(Color color) {
+  //   _currentColor = color;
+  //   notifyListeners();
+  // }
 
   /// Sets the active page and initializes data structures if needed.
   void setPage(int page) {
@@ -40,106 +40,106 @@ class DrawingController extends ChangeNotifier {
     notifyListeners();
   }
 
-  /// Starts a new drawing stroke at the given [startPoint].
-  void startDraw(Offset startPoint) {
-    _history.putIfAbsent(_currentPage, () => []);
-    _undoStack.putIfAbsent(_currentPage, () => []);
-    _history[_currentPage]!.add(SimpleLine(startPoint, _currentColor));
-    notifyListeners();
-  }
+  // /// Starts a new drawing stroke at the given [startPoint].
+  // void startDraw(Offset startPoint) {
+  //   _history.putIfAbsent(_currentPage, () => []);
+  //   _undoStack.putIfAbsent(_currentPage, () => []);
+  //   _history[_currentPage]!.add(SimpleLine(startPoint, _currentColor));
+  //   notifyListeners();
+  // }
 
-  /// Updates the current drawing stroke with a new point.
-  void drawing(Offset nowPaint) {
-    if (_history[_currentPage]?.isNotEmpty == true) {
-      _history[_currentPage]!.last.update(nowPaint);
-      notifyListeners();
-    }
-  }
+  // /// Updates the current drawing stroke with a new point.
+  // void drawing(Offset nowPaint) {
+  //   if (_history[_currentPage]?.isNotEmpty == true) {
+  //     _history[_currentPage]!.last.update(nowPaint);
+  //     notifyListeners();
+  //   }
+  // }
 
-  /// Ends the current drawing action.
-  void endDraw() {
-    notifyListeners();
-  }
+  // /// Ends the current drawing action.
+  // void endDraw() {
+  //   notifyListeners();
+  // }
 
-  /// Undoes the last drawing or action.
-  void undo() {
-    if (_history[_currentPage]?.isNotEmpty == true) {
-      var lastAction = _history[_currentPage]!.removeLast();
-      _undoStack[_currentPage]!.add(lastAction);
-      notifyListeners();
-    }
-  }
+  // /// Undoes the last drawing or action.
+  // void undo() {
+  //   if (_history[_currentPage]?.isNotEmpty == true) {
+  //     var lastAction = _history[_currentPage]!.removeLast();
+  //     _undoStack[_currentPage]!.add(lastAction);
+  //     notifyListeners();
+  //   }
+  // }
 
-  /// Redoes the last undone action.
-  void redo() {
-    if (_undoStack[_currentPage]?.isNotEmpty == true) {
-      var lastAction = _undoStack[_currentPage]!.removeLast();
-      _history[_currentPage]!.add(lastAction);
-      notifyListeners();
-    }
-  }
+  // /// Redoes the last undone action.
+  // void redo() {
+  //   if (_undoStack[_currentPage]?.isNotEmpty == true) {
+  //     var lastAction = _undoStack[_currentPage]!.removeLast();
+  //     _history[_currentPage]!.add(lastAction);
+  //     notifyListeners();
+  //   }
+  // }
 
-  /// Checks if there is content to undo or redo.
-  bool hasContent({bool isRedo = false}) {
-    if (isRedo) {
-      return _undoStack[_currentPage]?.isNotEmpty == true;
-    }
-    return _history[_currentPage]?.isNotEmpty == true;
-  }
+  // /// Checks if there is content to undo or redo.
+  // bool hasContent({bool isRedo = false}) {
+  //   if (isRedo) {
+  //     return _undoStack[_currentPage]?.isNotEmpty == true;
+  //   }
+  //   return _history[_currentPage]?.isNotEmpty == true;
+  // }
 
-  /// Checks if there is any content to clear (drawings, text, undo stack).
-  bool hasClearContent() {
-    return _history[_currentPage]?.isNotEmpty == true ||
-        _undoStack[_currentPage]?.isNotEmpty == true;
-  }
+  // /// Checks if there is any content to clear (drawings, text, undo stack).
+  // bool hasClearContent() {
+  //   return _history[_currentPage]?.isNotEmpty == true ||
+  //       _undoStack[_currentPage]?.isNotEmpty == true;
+  // }
 
-  /// Clears all drawings, text boxes, and undo stack for the current page.
-  void clear() {
-    _history[_currentPage] = [];
-    _undoStack[_currentPage] = [];
-    notifyListeners();
-  }
+  // /// Clears all drawings, text boxes, and undo stack for the current page.
+  // void clear() {
+  //   _history[_currentPage] = [];
+  //   _undoStack[_currentPage] = [];
+  //   notifyListeners();
+  // }
 
-  /// Clears all drawings, text boxes, and undo stacks for all pages.
-  void clearAllPages() {
-    _history.clear();
-    _undoStack.clear();
-    setPage(0);
-    notifyListeners();
-  }
+  // /// Clears all drawings, text boxes, and undo stacks for all pages.
+  // void clearAllPages() {
+  //   _history.clear();
+  //   _undoStack.clear();
+  //   setPage(0);
+  //   notifyListeners();
+  // }
 
-  bool hasAnyContent() {
-    return _history.values.any((actions) => actions.isNotEmpty);
-  }
+  // bool hasAnyContent() {
+  //   return _history.values.any((actions) => actions.isNotEmpty);
+  // }
 
-  /// Captures the current canvas as an image and returns it as [ByteData].
-  Future<ByteData?> getImageData() async {
-    try {
-      final RenderRepaintBoundary boundary =
-          painterKey.currentContext!.findRenderObject()!
-              as RenderRepaintBoundary;
-      final ui.Image originalImage = await boundary.toImage(pixelRatio: 3.0);
+  // /// Captures the current canvas as an image and returns it as [ByteData].
+  // Future<ByteData?> getImageData() async {
+  //   try {
+  //     final RenderRepaintBoundary boundary =
+  //         painterKey.currentContext!.findRenderObject()!
+  //             as RenderRepaintBoundary;
+  //     final ui.Image originalImage = await boundary.toImage(pixelRatio: 3.0);
 
-      final ui.PictureRecorder recorder = ui.PictureRecorder();
-      final Canvas canvas = Canvas(recorder);
-      final Paint paint = Paint();
-      canvas.drawImage(originalImage, Offset.zero, paint);
+  //     final ui.PictureRecorder recorder = ui.PictureRecorder();
+  //     final Canvas canvas = Canvas(recorder);
+  //     final Paint paint = Paint();
+  //     canvas.drawImage(originalImage, Offset.zero, paint);
 
-      final ui.Image finalImage = await recorder.endRecording().toImage(
-        originalImage.width,
-        originalImage.height,
-      );
-      return await finalImage.toByteData(format: ui.ImageByteFormat.png);
-    } catch (e) {
-      debugPrint('Error capturing or flipping image: $e');
-      return null;
-    }
-  }
+  //     final ui.Image finalImage = await recorder.endRecording().toImage(
+  //       originalImage.width,
+  //       originalImage.height,
+  //     );
+  //     return await finalImage.toByteData(format: ui.ImageByteFormat.png);
+  //   } catch (e) {
+  //     debugPrint('Error capturing or flipping image: $e');
+  //     return null;
+  //   }
+  // }
 
-  /// Returns all drawings across all pages.
-  Map<int, List<PaintContent>> getAllDrawings() {
-    return _history;
-  }
+  // /// Returns all drawings across all pages.
+  // Map<int, List<PaintContent>> getAllDrawings() {
+  //   return _history;
+  // }
 
   /// Adjusts page indexes after adding or removing a page.
   ///
@@ -185,36 +185,36 @@ class DrawingController extends ChangeNotifier {
   }
 }
 
-/// A simple freehand line composed of points and a color.
-class SimpleLine extends PaintContent {
-  /// List of points that form the line.
-  List<Offset> points = [];
+// /// A simple freehand line composed of points and a color.
+// class SimpleLine extends PaintContent {
+//   /// List of points that form the line.
+//   List<Offset> points = [];
 
-  /// Color of the line.
-  Color color;
+//   /// Color of the line.
+//   Color color;
 
-  /// Creates a new [SimpleLine] with a starting point and color.
-  SimpleLine(Offset startPoint, this.color) {
-    points.add(startPoint);
-  }
+//   /// Creates a new [SimpleLine] with a starting point and color.
+//   SimpleLine(Offset startPoint, this.color) {
+//     points.add(startPoint);
+//   }
 
-  @override
-  void update(Offset newPoint) {
-    points.add(newPoint);
-  }
+//   @override
+//   void update(Offset newPoint) {
+//     points.add(newPoint);
+//   }
 
-  @override
-  void paintOnCanvas(Canvas canvas) {
-    final paint =
-        Paint()
-          ..color = color
-          ..strokeWidth = 3
-          ..style = PaintingStyle.stroke;
-    for (int i = 0; i < points.length - 1; i++) {
-      canvas.drawLine(points[i], points[i + 1], paint);
-    }
-  }
-}
+//   @override
+//   void paintOnCanvas(Canvas canvas) {
+//     final paint =
+//         Paint()
+//           ..color = color
+//           ..strokeWidth = 3
+//           ..style = PaintingStyle.stroke;
+//     for (int i = 0; i < points.length - 1; i++) {
+//       canvas.drawLine(points[i], points[i + 1], paint);
+//     }
+//   }
+// }
 
 /// Abstract class representing any drawable content on the canvas.
 abstract class PaintContent {
